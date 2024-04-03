b2d9c04a9d928fecef5107d9894cd8efa8b224d8
zutin <gianluccaz@hotmail.com>
2024-04-03 10:58:22 -0300

    Listagem de filmes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   resources :genres, only: [:index, :show, :new, :create, :edit, :update]
   resources :directors, only: [:index, :show, :new, :create, :edit, :update]
-  resources :movies, only: [:show, :new, :create, :edit, :update]
+  resources :movies, only: [:index, :show, :new, :create, :edit, :update]
 end
